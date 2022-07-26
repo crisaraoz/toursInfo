b11717acab6c8877c1fc709f440f123cb66074ee
Cristian Iván Aráoz <araozcristian@gmail.com>
2022-07-25 21:06:11 -0300

    Initial Commit v.1

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>Loading Tours...</h1>
+    </div>
+  );
+};
+
+export default Loading;
+  
